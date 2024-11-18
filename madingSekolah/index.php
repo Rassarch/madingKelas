5579eb501aa98d74a934e34dda7d7c7492c88c8a
Rasssya <123206496+Rassarch@users.noreply.github.com>
2024-11-18 15:06:23 +0700

    Update index.php

--- madingSekolah/index.php
+++ madingSekolah/index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 include('konfigurasi/koneksi.php');
+
+// Ensure user session data is available
+$user_profile_image = isset($_SESSION['user']['profile_image']) ? $_SESSION['user']['profile_image'] : 'default-profile.png';
+$user_name = isset($_SESSION['user']['nama']) ? $_SESSION['user']['nama'] : 'Guest';
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +18,6 @@ include('konfigurasi/koneksi.php');
         .masonry {
             column-count: 3;
             column-gap: 1rem;
-            
         }
 
         .masonry-item {
@@ -23,28 +26,81 @@ include('konfigurasi/koneksi.php');
             border-radius: 5px;
             overflow: hidden;
         }
+
+        .profile-img {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            object-fit: cover;
+        }
+
+        .profile-info {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
+
+        .upload-btn {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0.5rem;
+            border-radius: 50%;
+            border: none;
+            background-color: #f8f9fa;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+        }
+
+        .upload-btn svg {
+            fill: #007bff;
+        }
+
+        .upload-btn:hover svg {
+            fill: #0056b3;
+        }
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="row mt-3">
-            <div class="col-md-3">
-                <?php if (isset($_SESSION['user'])) : ?>
-                <form action="proses/proses-logout.php" method="post">
-                    <button class="btn btn-danger">Logout</button>
-                </form>
-                <?php else : ?>
-                <a href="login.php" class="btn btn-primary">Login</a>
-                <?php endif ?>
-            </div>
-            <div class="col-md-6">
-                <h2 class="text-center">MADING SEKOLAH</h2>
-            </div>
-            <div class="col-md-3 text-end">
-                <a href="upload.php" class="btn btn-warning">Upload Mading</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand text-decoration-none" href="#">Mading Sekolah</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto align-items-center">
+                    <li class="nav-item">
+                        <a class="btn btn-light" href="upload.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                <path d="M8 1a.5.5 0 0 1 .5.5V7h5.5a.5.5 0 0 1 0 1H8.5v5.5a.5.5 0 0 1-1 0V8H2a.5.5 0 0 1 0-1h5.5V1.5A.5.5 0 0 1 8 1z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <?php if (isset($_SESSION['user'])) : ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="uploads/<?= $user_profile_image ?>" class="profile-img" alt="Profile Image">
+                            <?= $user_name ?>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item text-decoration-none" href="profile.php">Settings</a></li>
+                            <li>
+                                <form action="proses/proses-logout.php" method="post">
+                                    <button class="dropdown-item text-decoration-none" type="submit">Logout</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                    <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link text-decoration-none" href="login.php">Login</a>
+                    </li>
+                    <?php endif ?>
+                </ul>
             </div>
         </div>
-    </div>    
+    </nav>    
     <div class="container mt-4">
         <div class="masonry">
             <?php
@@ -54,7 +110,7 @@ include('konfigurasi/koneksi.php');
             foreach ($konten as $data):
             ?>
             <div class="masonry-item mb-2">
-                <a href="ditel.php?id=<?= $data['id'] ?>" class="link-secondary link-underline-opacity-0 link-offset-0">
+                <a href="ditel.php?id=<?= $data['id'] ?>" class="link-secondary link-underline-opacity-0 link-offset-0 text-decoration-none">
                     <img class="w-100" src="img/<?= $data['gambar']?>">
                     <div class="p-3">
                         <h4><?= $data['judul']?></h4>
